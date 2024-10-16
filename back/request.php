7438cf261df77daa937d2a260f19f3a584759012
MantYcore. <akshaymadathil3@gmail.com>
2024-10-16 23:55:42 +0530

    second commit

---
+++ back/request.php
@@ -0,0 +1,9 @@
+<?php 
+if(isset($_POST['signup'])) {
+  echo "User name is ".$_POST['username']."<br/>";
+  echo "Email name is ".$_POST['email']."<br/>";
+  echo "Password name is ".$_POST['password']."<br>";
+ 
+}
+
+?>
--- front/signup.php
+++ front/signup.php
@@ -1,17 +1,17 @@
 <div class="container">
   <h1 class="heading">signup</h1>
-  <form>
+  <form method="post" action="./back/request.php">
   <div class="col-6 offset-sm-3 mb-4">
-    <label for="name" class="form-label">Name</label>
-    <input type="name" class="form-control" id="exampleInputEmail1" placeholder="enter your Name">
+    <label for="name"  class="form-label" >Name</label>
+    <input type="name" class="form-control" name="username" id="exampleInputEmail1" placeholder="enter your Name">
     </div>
     <div class="col-6 offset-sm-3 mb-4">
     <label for="email" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="enter your email">
+    <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="enter your email">
     </div>
     <div class="col-6 offset-sm-3 mb-4">
     <label for="password" class="form-label">password</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="enter your password">
+    <input type="password" class="form-control" name="password"   id="exampleInputEmail1" placeholder="enter your password">
     </div>
   <!-- <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -19,7 +19,7 @@
   </div>
    -->
    <div class="col-6 offset-sm-3 mb-4">
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" name="signup" class="btn   btn-primary">Submit</button>
   </div>
 </form>
 
